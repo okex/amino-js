3828b348d2002c75739e79a12ebbca885b09242f
summerpro <974741468@qq.com>
2021-07-08 22:02:12 +0800

    add module types

---
+++ types/src/types/eth.d.ts
@@ -0,0 +1,11 @@
+export interface EthereumTx {
+    nonce: string;
+    gasPrice: string;
+    gas: string;
+    to: string;
+    value: string;
+    input: string;
+    v: string;
+    r: string;
+    s: string;
+}
