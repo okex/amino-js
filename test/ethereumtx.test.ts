e21dc50441003827a560186358e2052e163c7fd9
summerpro <974741468@qq.com>
2021-07-08 19:31:10 +0800

    optimize ut

--- test/ethereumtx.test.ts
+++ test/ethereumtx.test.ts
@@ -1,7 +1,7 @@
 import './setup';
 import * as Amino from '../';
 import SHA256 from "crypto-js/sha256";
-import hexEncoding from "crypto-js/enc-hex"
+import hexEncoding from "crypto-js/enc-hex";
 
 const ethereumTx = {
     nonce: "0",
@@ -9,7 +9,7 @@ const ethereumTx = {
     gas: "21000",
     to: "0xF76b10a1f318825173ad9F83f112e570782bD83E",
     value: "1000000000000000000000",
-    input: "",
+    input: "0x",
 
     v: "51",
     r: "96962823357929674581456147164326828477780712855340773268809156628353093709286",
